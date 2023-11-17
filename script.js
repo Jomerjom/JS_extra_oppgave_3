bed025d690f02e3e1a867f4d83ef70f8b99cefd2
Jomerjom <ekenesjoachim@gmail.com>
2023-11-17 09:01:22 +0100

    initial creation of repo

---
+++ script.js
@@ -0,0 +1,73 @@
+/*
+1.
+
+Use string methods on the text variable to do the following:
+ - Replace the word 'hard' with 'fun'
+ - Remove the whitespace from the beginning and end of the string
+ - Split the string into an array where each word is an element
+
+ Console log each method used.
+*/
+
+//! Code here
+
+/*
+2.
+
+Use array methods to do the following:
+ - Add a new hero to the end of the array
+ - Remove the first hero of the array (Spider-Man)
+ - Replace "Doctor Strange" with "Thanos"
+ - Use the splice method to remove Thor and Hulk and add "Captain America"
+*/
+
+//! Code here
+
+/*
+3.
+
+Write a function that takes in a string as a parameter and returns the string in upper
+case with an exclamation mark at the end. Use the non-arrow function syntax (function keyword).
+
+Use a template literal in your return statement.
+
+Example: console.log(yourFunction("this is cool")) should console log THIS IS COOL!
+Console log the function a few times to show that it's working.
+*/
+
+//! Code here
+
+/*
+4.
+
+Write a function that takes in an array as a parameter, and return the array with the first
+and last index removed. Use arrow function syntax.
+
+Example: console.log(yourFunction(["Red", "Green", "Blue", "Yellow"])) should console log
+an array with just ["Green", "Blue"]
+
+Console log the function a few times to show that it's working
+*/
+
+//! Code here
+
+/*
+5.
+
+EXTRA CHALLENGE:
+
+This one is not mandatory.
+
+Write a function that takes in 2 parameters, 1 array and 1 string.
+
+Inside your function, check if the array received contains the string received. If it does,
+remove that element from the array and return it.
+
+If the array does not contain the string, add that string to the end of the array and return it.
+
+Examples:
+console.log(yourFunction(["Red", "Green"], "Blue")) --> ["Red", "Green", "Blue"]
+console.log(yourFunction(["Red", "Green", "Blue"], "Green")) --> ["Red", "Blue"]
+*/
+
+//! Code here
